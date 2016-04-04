714656654297969eb0c055f6aa82e6afce7a12d0
Shawn Sandy <shawnsandy04@gmail.com>
2016-04-03 23:04:36 -0400

    First Commit

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use ShawnSandy\SimplePack\SimpleTest;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function(){
+    $test = new SimpleTest();
+    return $test->echoPhrase("test class");
+});
